50655898d989017c464fbfd02d57bba7311112ad
y4ngyy <591620892@qq.com>
2019-07-18 09:14:47 +0800

    修改home逻辑和admincheck

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -34,18 +34,21 @@
         name: "Admin",
         data: function() {
             return {
-                admin: true,
+                admin: false,
                 username:'请输入管理员用户名',
                 password:'请输入管理员密码',
             }
         },
         methods: {
             sendAdminInfo: function() {
-
+                this.$http.post('/api/admin-check', {
+                    username: this.username,
+                    password: this.password
+                }).then((response) => {
+                    console.log(response.data);
+                    this.admin = response.data.status === true;
+                })
             },
-            uploadFile: function () {
-
-            }
         }
     }
 </script>
